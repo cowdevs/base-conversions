db7b48e977dcf7605260188bc2169d061accd880
Luke L <chocopikachu752@gmail.com>
2024-11-09 00:26:33 -0800

    Initial commit

---
+++ src/main/java/net/cowdevs/baseconversions/BaseConversions.java
@@ -0,0 +1,16 @@
+package net.cowdevs.baseconversions;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class BaseConversions implements ModInitializer {
+	public static final String MOD_ID = "base-conversions";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
---
+++ src/main/java/net/cowdevs/baseconversions/BaseConversionsClient.java
@@ -0,0 +1,10 @@
+package net.cowdevs.baseconversions;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class BaseConversionsClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
